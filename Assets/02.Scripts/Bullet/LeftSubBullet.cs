a5e42d292d146d5c8f413e23f04705b3b4914da3
Moonto97 <kdypos@gmail.com>
2025-11-05 18:22:43 +0900

    11.05 2.번까지 구현함

---
+++ Assets/02.Scripts/Bullet/LeftSubBullet.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class LeftSubBullet : MonoBehaviour
+{
+    [Header("이동속력")]
+    public float StartSpeed = 1f;   // 초기 속도
+    private float _speed;   // 최종 속도
+    [Header("가속도")]
+    public float TargetSpeed = 4f;  // 목표 속도
+    public float TargetTime = 1.2f;     // 목표 속도 도달까지 걸리는 시간 (초)
+    [Header("발사각도")]
+    public float Angle = -0.25f;
+    [Header("데미지")]
+    public float BulletDamage = 40f;
+
+    public void Start()
+    {
+        _speed = StartSpeed;
+    }
+
+    public void Update()
+    {
+        float acceleration = (TargetSpeed - StartSpeed) / TargetTime; // 가속도 = (목표 속력 - 초기 속력) / 목표속력 달성까지 걸린 시간
+        _speed += Time.deltaTime * acceleration;  
+        _speed = Mathf.Min(_speed, TargetSpeed); // _speed 가 TargetSpeed 보다 커지면 TargetSpeed 로 고정
+
+        Vector2 direction = new Vector2(Angle, 1f); // 방향 백터 설정
+
+        Vector2 position = transform.position;  // 현재 위치
+
+
+        
+        Vector2 newPosition = position + direction * _speed * Time.deltaTime; // 새로운 위치 = 현재 위치 + 방향 벡터 * 속력 * 시간
+        transform.position = newPosition; // 위치 업데이트
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!other.CompareTag("Enemy")) return;
+        Enemy enemy = other.GetComponent<Enemy>();  // 충돌한 상대방 오브젝트의 Enemy 컴포넌트를 가져온다.
+        enemy.Hit(BulletDamage);  // 상대방 오브젝트의 Hit 메서드를 호출하면서 데미지 값을 전달한다.
+
+        Destroy(this.gameObject);
+    }
+}
+
--- Assets/02.Scripts/Player/PlayerFire.cs
+++ Assets/02.Scripts/Player/PlayerFire.cs
@@ -8,6 +8,8 @@ public class PlayerFire : MonoBehaviour
     // 필요 속성
     [Header("총알 프리팹")]
     public GameObject bulletPrefab;
+    public GameObject leftSubBulletPrefab;
+    public GameObject rightSubBulletPrefab;
     [Header("총구")]
     public Transform FirePosition;
     
@@ -20,42 +22,40 @@ public class PlayerFire : MonoBehaviour
 
     private void Update()
     {
-        Timer += Time.deltaTime;
-        
+        // 자동 발사    *** 처음에는 아래 코드를 if(Timer <= 0f)문 아래에 넣었었는데 
+        // 발사가 시작된 후에 멈추는 Alpha2 키 입력이 먹히지 않았음.
 
-        // 수동 발사
-        if (Input.GetKey(KeyCode.Space))
-        {
-            if (Timer >= CoolTime)
-            {
-                Fire();
-            }
-        }
-
-
-        // 자동 발사
-        if(Input.GetKeyDown(KeyCode.Alpha1))
+        // 원인 분석
+        // : Timer가 0 이하일 때만 if문 아래 코드가 실행되는데,
+        // AutoMode가 true가 되면 Timer가 계속 초기화되기 때문에
+        // "타이머가 Timer -= Time.deltaTime;에 의해 0이하가 되어
+        // 아래 코드가 실행되는 순간" ~ "발사되기 직전의 순간"
+        // 의 아주 짧은 한프레임의 순간에만 Alpha2 키 입력이 감지되는 것임.
+        // 그 한 프레임을 놓치면 타이머가 다시 초기화되기 때문에
+        // Alpha2 키 입력이 거의 먹히지 않는 것임.
+        if (Input.GetKeyDown(KeyCode.Alpha1))
         {
             AutoMode = true;
-            if (Timer >= CoolTime)
-            {  Fire(); 
-            }
+
         }
-        if(Input.GetKeyDown(KeyCode.Alpha2))
+        if (Input.GetKeyDown(KeyCode.Alpha2))
         {
             AutoMode = false;
-            if (Timer >= CoolTime)
-            {
-                Fire();
-            }
+
         }
 
 
+        Timer -= Time.deltaTime;
+        // 타이머가 계속 줄어들고, 0 이하일 때만 밑의 코드 실행(발사)
+        if (Timer >= 0f)
+        {
+            return;
+        }
 
-    }
+        
 
-    private void Fire()
-    {
+
+
         if (Input.GetKey(KeyCode.Space) || AutoMode)
         {
             // 2. 프리팹으로부터 게임 오버젝트를 생성한다.
@@ -71,10 +71,17 @@ public class PlayerFire : MonoBehaviour
             GameObject bullet3 = Instantiate(bulletPrefab);
             bullet3.transform.position = FirePosition.position + new Vector3(FireInterval, 0f, 0f);
 
-            Timer = 0f;
+            GameObject subBullet1 = Instantiate(leftSubBulletPrefab);
+            subBullet1.transform.position = FirePosition.position + new Vector3(-FireInterval * 2f, 0f, 0f);
+            GameObject subBullet2 = Instantiate(rightSubBulletPrefab);   
+            subBullet2.transform.position = FirePosition.position + new Vector3(FireInterval * 2f, 0f, 0f);
+
+            Timer = CoolTime;
 
         }
 
-
     }
+
+    
+
 }
